1a192e381a59a1349138ed1c48032e08fb34ecfe
yosuke1985 <ihatenonoildressing@gmail.com>
2018-10-11 18:03:01 +0900

    added Firebase_SMS_Auth

---
+++ Firebase_SMS_Auth/Firebase_SMS_AuthTests/Firebase_SMS_AuthTests.swift
@@ -0,0 +1,34 @@
+//
+//  Firebase_SMS_AuthTests.swift
+//  Firebase_SMS_AuthTests
+//
+//  Created by yosukenakayama on 2018/10/11.
+//  Copyright © 2018 yosuke. All rights reserved.
+//
+
+import XCTest
+@testable import Firebase_SMS_Auth
+
+class Firebase_SMS_AuthTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
